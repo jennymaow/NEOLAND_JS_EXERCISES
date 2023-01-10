13358b22e03a3eead2465a9d13ae41b365c49de5
jennymaow <maojenny172gmail.com>
2023-01-10 21:20:39 +0100

    Iteration#1 Iteration#2 Iteration#3

---
+++ JS_BASIC_ALGORITHMS/ITERACIÓN3_OPERADORES.js
@@ -0,0 +1,35 @@
+//1.1
+
+const mult = 10 * 5;
+console.log (mult);
+
+
+//1.2
+
+const div = 10 / 2;
+console.log (div);
+
+
+//1.3 
+
+const resto = 15 % 9;
+console.log (resto);
+
+
+//1.4
+
+let p = 10;
+const j = 5;
+p += j;
+const o = p;
+console.log (o);
+
+
+//1.5
+
+let c = 10;
+const m = 5;
+c *= m;
+const i = c;
+console.log (i);
+
