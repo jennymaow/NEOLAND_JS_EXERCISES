66dadbf4d253b7ee3c8c8dd3bf023f0f34b3d1b8
jennymaow <maojenny172gmail.com>
2023-01-10 22:20:44 +0100

    Iteration#1

---
+++ JS_LOOPS/ITERATION1.js
@@ -0,0 +1,26 @@
+const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta'];
+for ( let i = 0; i < products.length; i++){
+
+    const word = products[i];
+
+   if (word.includes ('Camiseta') === true){
+    console.log (products[i]);
+   }
+}
+
+
+/* const listFilms = ['Reservoir dogs', 'The Big Lewoski', 'End Game'];
+
+console.log(listFilms.includes('dogs'));
+
+Este sale false
+
+
+
+const frase = "Hasta el infinito y más allá!";
+console.log(frase.includes("infinito")); 
+
+Este sale true
+
+
+*/
