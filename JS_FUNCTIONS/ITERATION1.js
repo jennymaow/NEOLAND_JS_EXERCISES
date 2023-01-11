bc203409a0cf67d43a1420227e7dd5fce7305944
jennymaow <maojenny172gmail.com>
2023-01-11 17:46:20 +0100

    ITERATION#1

---
+++ JS_FUNCTIONS/ITERATION1.js
@@ -0,0 +1,13 @@
+function sum(numberOne , numberTwo) {
+    if (numberOne > numberTwo){
+        console.log (numberOne);
+        return numberOne;
+        
+    } else {
+        console.log (numberTwo);
+        return numberTwo;
+    }
+  }
+
+sum (2,1);
+sum (2,4);
