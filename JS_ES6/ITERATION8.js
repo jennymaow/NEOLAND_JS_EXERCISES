84b4e8e8dbcf30a1cce3d01288950782cfb130b6
jennymaow <maojenny172gmail.com>
2023-01-12 20:15:18 +0100

    ITERATION#8

--- JS_ES6/ITERATION8.js
+++ JS_ES6/ITERATION8.js
@@ -1,8 +1,21 @@
 const videogames = [
-    {name: 'Final Fantasy VII', genders: ['RPG'], score: 9.5},
-    {name: 'Assasins Creed Valhala', genders: ['Aventura', 'RPG'], score: 4.5},
-    {name: 'The last of Us 2', genders: ['Acción', 'Aventura'], score: 9.8},
-    {name: 'Super Mario Bros', genders: ['Plataforma'], score: 8.5},
-    {name: 'Genshin Impact', genders: ['RPG', 'Aventura'], score: 7.5},
-    {name: 'Legend of Zelda: Breath of the wild', genders: ['RPG', 'La cosa más puto bonita que he visto nunca'], score: 10},
-]
+  { name: "Final Fantasy VII", genders: ["RPG"], score: 9.5 },
+  { name: "Assasins Creed Valhala", genders: ["Aventura", "RPG"], score: 4.5 },
+  { name: "The last of Us 2", genders: ["Acción", "Aventura"], score: 9.8 },
+  { name: "Super Mario Bros", genders: ["Plataforma"], score: 8.5 },
+  { name: "Genshin Impact", genders: ["RPG", "Aventura"], score: 7.5 },
+  {
+    name: "Legend of Zelda: Breath of the wild",
+    genders: ["RPG", "La cosa más puto bonita que he visto nunca"],
+    score: 10,
+  },
+];
+
+const gameGenders = videogames.map (gender => gender.genders);
+console.log (gameGenders);
+
+const gendersRPG = videogames.find (genderRPG => genderRPG.genders ==="RPG");
+console.log (gendersRPG);
+
+
+
