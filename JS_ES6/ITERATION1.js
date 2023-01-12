6497b22b98c8b13ad9cf1abcef4ff9094e0c0a96
jennymaow <maojenny172gmail.com>
2023-01-12 16:05:15 +0100

    ITERATION#1

---
+++ JS_ES6/ITERATION1.js
@@ -0,0 +1,35 @@
+//1.1
+const a = 10;
+const b = 5;
+
+const suma = () => {
+    
+    return a + b; 
+}    
+
+let resultado = suma ();
+console.log (resultado);
+
+
+//1.2
+
+
+const suma2 = (a) => {
+    
+    return a + b; 
+}    
+
+let resultado2 = suma2 (a);
+console.log (resultado2);
+
+
+//1.3
+
+
+const suma3 = (a,b) => {
+    
+    return a + b; 
+}    
+
+let resultado3 = suma3 (a,b);
+console.log (resultado3);
