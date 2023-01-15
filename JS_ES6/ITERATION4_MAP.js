49c3fadf81219fa218564e63cf7b74ce123a7969
jennymaow <maojenny172gmail.com>
2023-01-15 21:05:29 +0100

    Weekend changes 3

--- JS_ES6/ITERATION4_MAP.js
+++ JS_ES6/ITERATION4_MAP.js
@@ -13,13 +13,19 @@ console.log(names);
 
 //4.2
 
+
 for (name1 of names) {
+
   const letters = [...name1];
   /* console.log (letters);
-    console.log (letters[0]); */
-  if (letters[0] === "A") {
+  console.log (letters[0]); */
+
+  if (letters[0] == "A") {
     name1 = "Anacleto";
-  }
+  } 
+
+  /* const userNames = letters.map ((letter)=> (letter == "A" ? nameA = "Anacleto" : nameA=nameA)); */
+
 }
 
 console.log(names);
@@ -34,15 +40,16 @@ const cities = [
 ];
 
 const visitedCities = cities.map (visited => visited.isVisited);
-console.log (visitedCities);
+//console.log (visitedCities);
 
 const namesCities = cities.map((city) => (city.name));
-console.log(namesCities);
+//console.log(namesCities);
 
 for (let i=0; i<visitedCities.length;i++){
   if (visitedCities[i]){
     namesCities[i]= namesCities[i] + " (Visitado)";
   }
 }
+
 
 console.log (namesCities);
--- README.md
+++ README.md
@@ -32,7 +32,7 @@ ES6
 Iteration#1
 Iteration#2 
 Iteration#3
-Iteration#4
+Iteration#4 4.2?
 Iteration#5
 Iteration#6 
 Iteration#7
