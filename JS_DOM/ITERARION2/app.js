090c84cb0f2ab77e3712e65babb1aea3e7363ac8
jennymaow <maojenny172gmail.com>
2023-01-25 17:35:53 +0100

    DOM exercises

---
+++ JS_DOM/ITERARION2/app.js
@@ -0,0 +1,69 @@
+let newDiv = document.createElement("div");
+let newDiv2 = document.createElement("div");
+let newP = document.createElement("p");
+let myH2 = document.querySelector("h2");
+
+newDiv.appendChild(newP);
+document.body.insertBefore(newDiv, myH2);
+console.log(newDiv);
+
+document.body.insertBefore(newDiv2, myH2);
+
+//2.3
+
+console.log(newDiv2);
+/*  for (let i = 0; (i = 5); i++) {
+  newDiv2.insertAdjacentHTML("afterbegin", "<p></p>");
+}  */
+
+//2.4
+myH2.insertAdjacentHTML("beforebegin", "<p>Soy dinámico!</p>");
+
+//2.5
+let textH2 = document.createTextNode("Wubba Lubba dub dub");
+
+myH2.appendChild(textH2);
+
+//2.6
+
+const apps = ["Facebook", "Netflix", "Instagram", "Snapchat", "Twitter"];
+
+let newList = document.createElement("ul");
+document.body.insertBefore(newList, myH2);
+
+const listArray = (array) => {
+  for (item of array) {
+    newList.insertAdjacentHTML("afterbegin", `<li>${item}</li>`);
+  }
+};
+
+listArray(apps);
+
+//2.7
+
+const removeClass = document.querySelectorAll(".fn-remove-me");
+
+for (let item of removeClass) {
+  item.remove();
+}
+
+//2.8
+let newPDiv = document.createElement("p");
+let textDiv = document.createTextNode("Voy en medio!");
+
+let divInsert = document.querySelectorAll("div.fn-insert-here");
+newPDiv.appendChild(textDiv);
+document.body.insertBefore(newPDiv, divInsert[1]);
+
+//2.9
+
+let newText3 = document.createTextNode("Voy dentro!");
+let newP3 = document.createElement("p");
+
+newP3.appendChild(newText3);
+console.log(newP3);
+console.log(divInsert);
+
+for (let item of divInsert) {
+  item.insertAdjacentHTML("afterbegin", "<p>Voy dentro!</p>");
+}
---
+++ JS_DOM/ITERATION1/app.js
@@ -0,0 +1,19 @@
+const myShowMe = document.querySelector (".showme");
+
+console.log (myShowMe);
+
+const myNewH1= document.querySelector ("#pillado");
+
+console.log (myNewH1);
+
+const myP= document.querySelectorAll("p");
+console.log (myP);
+
+
+const myPokemon = document.querySelectorAll(".pokemon");
+console.log (myPokemon);
+
+const myTestMe = document.querySelectorAll('span[data-function="testMe"]');
+
+console.log (myTestMe);
+console.log (myTestMe[3]);
